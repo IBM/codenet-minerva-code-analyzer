30a8700b69fd318254fcf47d5c455a6c8a967039
Rahul Krishna <rkrsn@ibm.com>
2024-04-24 17:30:35 -0400

    Update maven build to add '-Drat.skip'
    Signed-off-by: Rahul Krishna <rkrsn@ibm.com>

--- src/main/java/com/ibm/northstar/utils/BuildProject.java
+++ src/main/java/com/ibm/northstar/utils/BuildProject.java
@@ -70,7 +70,7 @@ public class BuildProject {
             return false;
         }
         String[] mavenCommand = {
-                    "mvn", "clean", "package", "-f", projectPath + "/pom.xml", "-B", "-V", "-e",
+                    "mvn", "clean", "package", "-f", projectPath + "/pom.xml", "-B", "-V", "-e", "-Drat.skip",
                     "-Dfindbugs.skip", "-Dcheckstyle.skip", "-Dpmd.skip=true", "-Dspotbugs.skip", "-Denforcer.skip",
                     "-Dmaven.javadoc.skip", "-DskipTests", "-Dmaven.test.skip.exec", "-Dlicense.skip=true",
                     "-Drat.skip=true", "-Dspotless.check.skip=true" };
