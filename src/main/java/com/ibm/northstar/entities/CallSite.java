036fc671b17cfde72668eadfc26aabdb5c42af17
Saurabh Sinha <sinha108@gmail.com>
2024-04-24 15:27:10 -0400

    Added information about call sites to callable
    Signed-off-by: Saurabh Sinha <sinha108@gmail.com>

---
+++ src/main/java/com/ibm/northstar/entities/CallSite.java
@@ -0,0 +1,16 @@
+package com.ibm.northstar.entities;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CallSite {
+    private String methodName;
+    private String declaringType;
+    private List<String> argumentTypes;
+    private int startLine;
+    private int startColumn;
+    private int endLine;
+    private int endColumn;
+}
