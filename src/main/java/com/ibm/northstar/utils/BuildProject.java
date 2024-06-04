dce275560159b07bd9345c31888ce4d100b41ed8
Rahul Krishna <i.m.ralk@gmail.com>
2024-06-04 13:53:42 -0400

    During build, package stage is not necessary, so we switched to doing only compile.OC
    Signed-off-by: Rahul Krishna <i.m.ralk@gmail.com>

--- src/main/java/com/ibm/northstar/utils/BuildProject.java
+++ src/main/java/com/ibm/northstar/utils/BuildProject.java
@@ -73,7 +73,7 @@ public class BuildProject {
             return false;
         }
         String[] mavenCommand = {
-                    MAVEN_CMD, "clean", "package", "-f", projectPath + "/pom.xml", "-B", "-V", "-e", "-Drat.skip",
+                    MAVEN_CMD, "clean", "compile", "-f", projectPath + "/pom.xml", "-B", "-V", "-e", "-Drat.skip",
                     "-Dfindbugs.skip", "-Dcheckstyle.skip", "-Dpmd.skip=true", "-Dspotbugs.skip", "-Denforcer.skip",
                     "-Dmaven.javadoc.skip", "-DskipTests", "-Dmaven.test.skip.exec", "-Dlicense.skip=true",
                     "-Drat.skip=true", "-Dspotless.check.skip=true" };
